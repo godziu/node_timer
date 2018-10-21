1609146011159c99c236b82df0367f86b7531c7f
Damian Godek <damian.godek1@gmail.com>
2018-10-22 00:34:56 +0200

    delete OSinfo problems

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var timeConvert = require('../modules/TimeConvert');
 
 function getOSinfo() {
     var type = os.type();
@@ -14,7 +15,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    timeConvert.print();
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
